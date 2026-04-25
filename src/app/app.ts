c9dcf971614ca8f2289a62ef3fdd6a057bf581b2
Alvaro Quiroz <Alvaro Quiroz>
2026-04-25 10:36:56 -0500

    feat: inicializacion del proyecto Angular TF

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('UPC-202601-1931-G01-TF');
+}
